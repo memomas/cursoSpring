767db792f573c53052a553ef36c0c8cad97870a4
Guillermo Mascote <gmascote@cinepolis.com>
2021-03-07 21:01:07 -0600

    Creando CRUD api rest

---
+++ src/main/java/com/cnpls/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.cnpls.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cnpls.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Guillermo', 'Mascote', 'gmascote@cinepolis.com', '2019-10-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Adolfo', 'Mascote', 'amascote@cinepolis.com', '2019-10-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nancy', 'Mascote', 'nmascote@cinepolis.com', '2019-10-29');
